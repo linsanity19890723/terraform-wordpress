8ea8413b0cb9616f2ea8a3d9507d3d7decba8224
Ubuntu <linsanity19890723@gmail.com>
2021-09-09 02:37:27 +0000

    git

---
+++ providers.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region     = "us-east-2"
+
+}
+
+# provider "template" {
+#   # version = "~> 2.1.2"
+# }
---
+++ vars.tf
@@ -0,0 +1,34 @@
+variable "aws_reg" {
+  description = "This is aws region"
+  default     = "eu-west-2"
+  type        = string
+}
+
+variable "stack" {
+  description = "this is name for tags"
+  default     = "terraform"
+}
+
+variable "username" {
+  description = "DB username"
+}
+
+variable "password" {
+  description = "DB password"
+}
+
+variable "dbname" {
+  description = "db name"
+}
+
+variable "ssh_key" {
+  # default     = "~/.ssh/id_rsa.pub"
+  default     = "./xxx.pub"
+  description = "Default pub key"
+}
+
+variable "ssh_priv_key" {
+  # default     = "~/.ssh/id_rsa"
+  default     = "./test"
+  description = "Default pub key"
+}
